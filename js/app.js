253074071e0e5f3a76aaa1e979b47fefe5e308f4
Paniz Ahang <pahang@ucvts.org>
2020-01-03 10:25:39 -0500

    why

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("works")
+const items = [];
+
+window.onload = function() {
+    document.getElementById("add-button").onclick = addItem;
+};
+
+const addItem = function() {
+  const text = document.getElementById("theInput").value;
+
+  items.push({
+    priority: "low",
+    complete: false,
+    content: text
+  });
+
+  document.getElementById("theInput").value = "";
+
+  renderItems();
+};
+
+const renderItems = function() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  for (let i = 0; i < items.length; i++) {
+    const li = document.createElement("li");
+
+    const div = document.createElement("div");
+    div.innerHTML = items[i].content;
+
+    li.append(div);
+    list.append(li);
+  }
+};
